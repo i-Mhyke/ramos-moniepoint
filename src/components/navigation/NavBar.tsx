d2e09b2cc4af68363b690ed6f2db1cdce6607a1d
Michael Madumere <ihunayamadu@gmail.com>
2024-05-24 23:03:18 +0100

    update navbar

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -148,36 +148,36 @@ export const BottomBar = () => {
                 {
                     show && (
                         <>
-                            <AppContainer className="fixed bottom-10 left-1/2 -translate-x-1/2 z-20">
-                                <Animate  viewDelay={0} once={false}
-                                    animation={{
-                                        hidden: {
-                                            y: 100,
-                                        },
-                                        visible: {
-                                            y: 0,
-                                            transition: {
-                                                duration: 2.5,
-                                                ease: "easeInOut",
-                                                type: "spring",
-                                                bounce: 0.2,
-                                            }
-                                        },
-                                    }}
-                                >
-                                <div className="flex justify-between w-fit mx-auto bg-dark text-white rounded-2xl py-2 pl-3 pr-2 mt-5 overflow-hidden">
-                                        <div className="bg-[rgb(37,37,37)] rounded-xl px-4 py-3">
-                                            <ul className="flex items-center space-x-7">
-                                                {navList.map((nav, index) => (
-                                                    <li key={index} className="">
-                                                        <a href={nav.path} className="text-sm hover:text-primary p-1 transition-all duration-300">{nav.name}</a>
-                                                    </li>
-                                                ))}
-                                            </ul>
-                                        </div>
-                                </div>
-                                </Animate>
-                            </AppContainer>
+                        <AppContainer className="fixed bottom-10 left-1/2 -translate-x-1/2 z-20">
+                            <Animate  viewDelay={0} once={false}
+                                animation={{
+                                    hidden: {
+                                        y: 100,
+                                    },
+                                    visible: {
+                                        y: 0,
+                                        transition: {
+                                            duration: 2.5,
+                                            ease: "easeInOut",
+                                            type: "spring",
+                                            bounce: 0.2,
+                                        }
+                                    },
+                                }}
+                            >
+                            <div className="flex justify-between w-fit mx-auto bg-dark text-white rounded-2xl py-2 pl-3 pr-2 mt-5 overflow-hidden">
+                                    <div className="bg-[rgb(37,37,37)] rounded-xl px-4 py-3">
+                                        <ul className="flex items-center space-x-7">
+                                            {navList.map((nav, index) => (
+                                                <li key={index} className="">
+                                                    <a href={nav.path} className="text-sm hover:text-primary p-1 transition-all duration-300">{nav.name}</a>
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                            </div>
+                            </Animate>
+                        </AppContainer>
                     </>
                     )
                 }
